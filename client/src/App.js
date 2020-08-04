abdd3641f5e8bc83f7a69f2ae1fa4678bde3a516
Brian Tan <briantanw@gmail.com>
2020-08-03 20:20:35 -0700

    added client folder

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PostCreate from './PostCreate';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Create Post</h1>
+      <PostCreate />
+    </div>
+  );
+}
+
+export default App;
