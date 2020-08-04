88ec29136c424433096195d206e162e72cf6eadd
Brian Tan <briantanw@gmail.com>
2020-08-03 22:13:01 -0700

    completed React end

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PostCreate from './PostCreate';
+import PostList from './PostList';
 import './App.css';
 
 function App() {
@@ -7,6 +8,9 @@ function App() {
     <div className="container">
       <h1>Create Post</h1>
       <PostCreate />
+      <hr />
+      <h1>Posts</h1>
+      <PostList />
     </div>
   );
 }
